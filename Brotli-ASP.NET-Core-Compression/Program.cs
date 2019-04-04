4ef22cc1b250d53b6b0d1e12f42e154235fb9046
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-04-04 22:07:57 +0200

    Upgraded from .NET Core 1.1 to .NET Core 2.2

--- Brotli-ASP.NET-Core-Compression/Program.cs
+++ Brotli-ASP.NET-Core-Compression/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Brotli_ASP.NET_Core_Compression
@@ -11,15 +7,14 @@ namespace Brotli_ASP.NET_Core_Compression
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+            BuildWebHost(args).Run();
+        }
 
-            host.Run();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
         }
     }
 }
